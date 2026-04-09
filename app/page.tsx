b7746876c6bf2ed2c2af833eb852f4b527b1ec9e
abhi <abhiifour@gmail.com>
2026-04-09 12:57:23 +0530

    fix: reduce padding

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="w-full bg-[#F4F4F5] py-8 md:py-12 flex justify-center font-poppins">
+    <div className="w-full bg-[#F4F4F5] md:py-12 flex justify-center font-poppins">
       {/* Main */}
       <div className="p-2 md:p-4 w-full max-w-xl flex flex-col gap-4 bg-[#FEFFFF] rounded-xl shadow-2xl">
         <Hero />
