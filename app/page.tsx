02e42413a7b638190dfdf008eb46431a9943095c
abhi <abhiifour@gmail.com>
2026-04-10 12:33:27 +0530

    add animation :)

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,77 @@
+"use client"
 import Contact from "@/components/Contact";
 import Experience from "@/components/Experience";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import StackAndSkills from "@/components/StackAndSkills";
-// import Image from "next/image";
+import {motion} from 'motion/react'
+
 
 export default function Home() {
+
+
+  const fadeUpVariants: any = {
+    hidden: {
+      opacity: 0,
+      y: 20,
+      filter: 'blur(6px)',
+    },
+    visible: (i: any) => ({
+      opacity: 1,
+      y: 0,
+      filter: 'blur(0px)',
+      transition: {
+        duration: 0.8,
+        delay: i * 0.15,
+        ease: [0.25, 0.4, 0.3, 1],
+      },
+    }),
+  };
+
   return (
     <div className="w-full bg-[#F4F4F5] md:py-12 flex justify-center font-poppins">
       {/* Main */}
       <div className="p-2 md:p-4 w-full max-w-xl flex flex-col gap-4 bg-[#FEFFFF] rounded-xl shadow-2xl">
-        <Hero />
-        <Experience />
-        <StackAndSkills />
-        <Projects />
-        <Contact />
+        <motion.div
+        variants={fadeUpVariants}
+        initial="hidden"
+        animate="visible"
+        custom={1}
+        >
+          <Hero />
+        </motion.div>
+        <motion.div
+         variants={fadeUpVariants}
+        initial="hidden"
+        animate="visible"
+        custom={2}
+        >
+          <Experience />
+        </motion.div>
+        <motion.div
+         variants={fadeUpVariants}
+              initial="hidden"
+              animate="visible"
+              custom={3}
+        >
+          <StackAndSkills />
+        </motion.div>
+        <motion.div
+         variants={fadeUpVariants}
+              initial="hidden"
+              animate="visible"
+              custom={4}
+        >
+          <Projects />
+        </motion.div>
+        <motion.div
+         variants={fadeUpVariants}
+              initial="hidden"
+              animate="visible"
+              custom={5}
+        >
+          <Contact />
+        </motion.div>
       </div>
     </div>
   );
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,5 +1,7 @@
+'use client'
 // import ExperienceCard from "./ExperienceCard";
 import ProjectCard from "./ProjectCard";
+import {motion} from 'motion/react'
 
 const projects =[
     
@@ -33,13 +35,39 @@ const projects =[
 ]
 
 export default function Projects() {
+
+    const fadeUpVariants: any = {
+        hidden: {
+            opacity: 0,
+            y: 40,
+            filter: 'blur(2px)',
+        },
+        visible: (i: any) => ({
+            opacity: 1,
+            y: 0,
+            filter: 'blur(0px)',
+            transition: {
+                duration: 0.2,
+                delay: i * 0.4,
+                type: 'spring'
+            },
+        }),
+    };
     return (
         <div className="bg-[#F2F2F2] p-6 rounded-xl border border-black/4">
             <h2 className="uppercase text-md font-medium">Projects</h2>
             <div className="flex flex-col gap-16 justify-between mt-6">
 
-                {
-                    projects.map((project: any) => <ProjectCard key={project.name} name={project.name} details={project.details} sector={project.sector} rotation={project.rotation} className={project.className}/>)
+                {   
+                    projects.map((project: any, index: number) => <motion.div
+                    variants={fadeUpVariants}
+                    initial="hidden"
+                    animate='visible'
+                    key={project.name}
+                    custom={index+1}
+                    >
+                        <ProjectCard  name={project.name} details={project.details} sector={project.sector} rotation={project.rotation} className={project.className}/>
+                    </motion.div>)
                 }
 
 
